b9685d0fed8486eb9f8b5b4297b6e5c2c02b9eab
JoshuaHolloway <jhollow6@asu.edu>
2021-09-16 23:54:51 -0500

    added nav links

--- frontend/src/shared/components/Navigation/MainNavigation.js
+++ frontend/src/shared/components/Navigation/MainNavigation.js
@@ -1,3 +1,6 @@
+import { Link } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
+
 import MainHeader from './MainHeader';
 
 import classes from './MainNavigation.module.css';
@@ -8,7 +11,36 @@ const MainNavigation = (props) => {
   return (
     <>
       <MainHeader>
-        <h1>MainHeader Content</h1>
+        <button>
+          <span></span>
+          <span></span>
+          <span></span>
+        </button>
+        <h1>
+          <Link>Water Your Plants</Link>
+        </h1>
+
+        <nav>
+          <ul>
+            <li>
+              <NavLink to='/' exact>
+                Home
+              </NavLink>
+            </li>
+
+            <li>
+              <NavLink to={`/:userId/plants`}>/:userId/plants</NavLink>
+            </li>
+
+            <li>
+              <NavLink to={'plants/new'}>/plants/new</NavLink>
+            </li>
+
+            <li>
+              <NavLink to={'/auth'}>AUTHENTICATE</NavLink>
+            </li>
+          </ul>
+        </nav>
       </MainHeader>
     </>
   );
