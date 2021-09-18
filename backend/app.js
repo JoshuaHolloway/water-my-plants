58c3c7869b6dc28e50165c39fdd5ca0b8974715d
JoshuaHolloway <jhollow6@asu.edu>
2021-09-18 02:21:54 -0500

    handling GET requests to /api/plants/user/:uid

--- backend/app.js
+++ backend/app.js
@@ -7,7 +7,7 @@ const app = express();
 
 // -Add the routes (from plants-router.js)
 //  to middleware in app.js
-app.use('/api/places', plantsRoutes);
+app.use('/api/plants', plantsRoutes);
 // -Using the optional first string arguement here
 //  makes express only forward requests to plantsRoutes
 //  middleware if their path starts with /api/places
--- backend/routes/plants-routes.js
+++ backend/routes/plants-routes.js
@@ -4,10 +4,48 @@ const router = express.Router();
 
 // ==============================================
 
-// /api/places/
+const DUMMY_PLANTS = [
+  {
+    id: 'p1', // o  id
+    nickname: '', // o  Nickname
+    species: 'species A', // o  Species
+    h20Frequency: 2, // o  h20Frequency [units???]
+    image: '', // o  Image.
+    creator: 'u1',
+  },
+];
+
+// ==============================================
+
+// /api/plants/
 router.get('/', (req, res) => {
   console.log('GET request in places');
   res.send({ message: 'Do epic shit!' });
+});
+
+// ==============================================
+
+// /api/plants/p1
+router.get('/:pid', (req, res) => {
+  const plantId = req.params.pid; // { pid: 'p1' }
+
+  const plant = DUMMY_PLANTS.find((p) => p.id === plantId);
+
+  console.log('GET request in places');
+  res.json({ plant });
+});
+
+// ==============================================
+
+// /api/plants/user/u1
+// o  (GET) /api/plants
+// 	§ Retrieve list of all plants for currently logged in user
+router.get('/user/:uid', (req, res) => {
+  const userId = req.params.uid;
+
+  const plant = DUMMY_PLANTS.find((p) => p.creator === userId);
+
+  res.json({ plant });
 });
 
 // ==============================================
