a7a2a4fd3a8d0ab10365c34ca96d158ec0567f7d
JoshuaHolloway <jhollow6@asu.edu>
2021-09-18 02:52:35 -0500

    Handling errors

--- backend/app.js
+++ backend/app.js
@@ -12,4 +12,29 @@ app.use('/api/plants', plantsRoutes);
 //  makes express only forward requests to plantsRoutes
 //  middleware if their path starts with /api/places
 
+// -Error handling middleware
+// -Four args => special middleware function
+//               used as error middleware function.
+// -The callback function will run if any middleware
+//  in front of this yields an error.
+app.use((error, req, res, next) => {
+  // -Check to see if response has already
+  //  been sent.
+  // -If that is the case, then we want to
+  //  return next, and forward the error.
+  // -In other words, we won't send a response
+  //  on our own, because somehow we
+  //  already have sent a response.
+  if (res.headerSent) {
+    return next(error);
+  }
+
+  // -Set status code on response
+  // -If code property on error object passed in
+  //  then use it,
+  //  else, set 500 status.
+  res.status(error.code || 500);
+  res.json({ message: error.message || 'An unknown error occured!' });
+});
+
 app.listen(5e3, () => console.log('localhost:5000'));
--- backend/routes/plants-routes.js
+++ backend/routes/plants-routes.js
@@ -27,11 +27,32 @@ router.get('/', (req, res) => {
 
 // /api/plants/p1
 router.get('/:pid', (req, res) => {
+  console.log('/api/plants/:pid');
+
   const plantId = req.params.pid; // { pid: 'p1' }
 
   const plant = DUMMY_PLANTS.find((p) => p.id === plantId);
 
-  console.log('GET request in places');
+  if (!plant) {
+    // -did not find resource
+    // return res
+    //   .status(404)
+    //   .json({ message: 'could not find a plant for the provided id!' });
+
+    // -throwing error here will jump code execution
+    //  to the error handling middleware in app.js.
+    // throw new Error();
+    // -Calling next() with Error passed as arg
+    //  also does same thing (with one difference:
+    //  you cannot throw errors in async code [this is synchronous]).
+    // next(error);
+
+    const error = new Error(
+      'Could not find a plant for the provided PLANT id!  (string arg to Error object constructor in .get(/:pid))'
+    );
+    error.code = 404;
+    throw error;
+  }
   res.json({ plant });
 });
 
@@ -40,11 +61,23 @@ router.get('/:pid', (req, res) => {
 // /api/plants/user/u1
 // o  (GET) /api/plants
 // 	§ Retrieve list of all plants for currently logged in user
-router.get('/user/:uid', (req, res) => {
+router.get('/user/:uid', (req, res, next) => {
   const userId = req.params.uid;
 
   const plant = DUMMY_PLANTS.find((p) => p.creator === userId);
-
+  if (!plant) {
+    // return res
+    //   .status(404)
+    //   .json({ message: 'could not find a plant for the provided user id' });
+    const error = new Error(
+      'Could not find a plant for the provided USER id!  (string arg to Error object constructor in .get(/user/:uid)'
+    );
+    error.code = 404;
+    // throw error;
+    return next(error);
+    // -return to avoid sending response outside of this if block.
+    // -Returned value not used though.
+  }
   res.json({ plant });
 });
 
