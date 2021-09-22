5ad1b290245965ec3381cabfae1ad9c3b2292767
JoshuaHolloway <jhollow6@asu.edu>
2021-09-21 23:43:32 -0500

    directory refactor (controllers -> users-controller -> index.js)

--- backend/routes/users-routes.js
+++ backend/routes/users-routes.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const { check } = require('express-validator');
 
-const usersControllers = require('../controllers/users-controller/users-controller');
+const usersControllers = require('../controllers/users-controller');
 const router = express.Router();
 
 // ==============================================
